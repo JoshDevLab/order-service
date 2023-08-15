870723c3d56dba7d83b0f391b3c315c243b43e0e
josh <tkddns6007@gmail.com>
2023-08-15 17:04:51 +0900

    dto, service 생성

--- src/main/java/com/example/orderservice/jpa/OrderRepository.java
+++ src/main/java/com/example/orderservice/jpa/OrderRepository.java
@@ -3,5 +3,6 @@ package com.example.orderservice.jpa;
 import org.springframework.data.repository.CrudRepository;
 
 public interface OrderRepository extends CrudRepository<OrderEntity, Long> {
-    OrderEntity findByProductId(String productId);
+    OrderEntity findByOrderId(String OrderId);
+    Iterable<OrderEntity> findByUserId(String userId);
 }
