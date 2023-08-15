0ff3dadbf7add4db1ce5c6cd99338312da1cd9fd
josh <tkddns6007@gmail.com>
2023-08-15 16:39:57 +0900

    order 엔티티 및 레파지토리 생성

---
+++ src/main/java/com/example/orderservice/jpa/OrderRepository.java
@@ -0,0 +1,7 @@
+package com.example.orderservice.jpa;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface OrderRepository extends CrudRepository<OrderEntity, Long> {
+    OrderEntity findByProductId(String productId);
+}
